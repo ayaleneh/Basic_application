f23d93b08b41c615ca9de8251a8b46a3bc3c1335
ayaleneh <getaunayaleneh@gmail.com>
2019-01-29 09:38:50 -0500

    basic application

---
+++ src/Basic_applocation.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Basic_applocation {
+    public static void main(String[] args) {
+        int number;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("please enter a integer");
+              number=scanner.nextInt();
+              number=number+5;
+              number=number*2;
+              number=number-7;
+              System.out.println("the final result is "+number);
+    }
+}
